5074e158ef6c7b8ab260e612c2f3f91926a76389
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-08 15:09:06 +0530

    Added removing functionality

--- JS1/js1.js
+++ JS1/js1.js
@@ -1,3 +1,6 @@
+const del = '<button onclick="removeItem(event, this)"><img src="./delete.png" alt="delete"></button>';
+const edit = '<button onclick="editItem(event, this)"><img src="./edit.svg" alt="edit"></button>';
+
 function handleKeyPress(e) {
 	if (e.keyCode === 13) {
 		e.preventDefault();
@@ -27,10 +30,30 @@ function completeItem() {
 	target.insertBefore(item, target.childNodes[0]);
 }
 
+function removeItem(e, it) {
+	e.stopPropagation();
+	let item = it.parentNode.parentNode;
+	let parent = item.parentNode;
+	//console.log(parent.id);
+	
+	parent.removeChild(item);
+	console.log("removed");
+}
+
+function editItem(e, it) {
+	e.stopPropagation();
+	console.log("edited");
+}
+
 function addItem(text) {
 	let list = document.getElementById('todo');
 	let item = document.createElement('li');
 	item.innerText = text;
+	
+	let buttons = document.createElement('span');
+	buttons.innerHTML = del+edit;
+	
+	item.appendChild(buttons);
 	
 	item.addEventListener('click', completeItem);
 	
