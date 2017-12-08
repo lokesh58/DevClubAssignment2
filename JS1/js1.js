e9729fc39d20ee29d874e13f38ce2b9196dd17f4
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-08 12:38:07 +0530

    Prepared basic input of todo item. Currently it goes to console

--- JS1/js1.js
+++ JS1/js1.js
@@ -1,1 +1,15 @@
-console.log("javascipt working");
+function handleKeyPress(e) {
+	if (e.keyCode === 13) {
+		e.preventDefault();
+		addToList();
+	}
+}
+
+function addToList() {
+	let value = document.getElementById('item').value;
+	
+	if (value) {
+		console.log(value);
+		document.getElementById('item').value = '';
+	}
+}
