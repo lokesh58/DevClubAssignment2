9b9d878fc9c55ea6b5a78ad3b6dcb763a023600f
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-08 13:32:40 +0530

    Now we can complete and uncomplete items

--- JS1/js1.js
+++ JS1/js1.js
@@ -9,16 +9,30 @@ function addToList() {
 	let value = document.getElementById('item').value;
 	
 	if (value) {
-		console.log(value);
+		//console.log(value);
 		document.getElementById('item').value = '';
 		addItem(value);
 	}
+}
+
+function completeItem() {
+	let item = this;
+	let parent = item.parentNode;
+	let id = parent.id;
+	let target = (id == 'todo' ? document.getElementById('completed') : document.getElementById('todo'));
+	
+	//console.log(parent.id);
+	
+	parent.removeChild(item);
+	target.insertBefore(item, target.childNodes[0]);
 }
 
 function addItem(text) {
 	let list = document.getElementById('todo');
 	let item = document.createElement('li');
 	item.innerText = text;
+	
+	item.addEventListener('click', completeItem);
 	
 	list.insertBefore(item, list.childNodes[0]);
 }
